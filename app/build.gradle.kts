4a1ae7c4d47f95ccf8e8f5f96352115549823c89
LeanBitLab <Leanbitlab@gmail.com>
2026-08-23 02:16:03 +0530

    chore(release): bump version to v1.0.1 (101)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.leanbitlab.leantype.voice.offline"
         minSdk = 21
         targetSdk = 35
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
 
         externalNativeBuild {
             cmake {
